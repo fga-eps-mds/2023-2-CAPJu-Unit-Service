74b85c1919d9ee3114e2a1b5b4a7be51ce805bda
ngm1450 <georgeos.nicolas83@gmail.com>
2023-12-04 09:41:22 -0300

    correção middleware

--- src/services/_index.js
+++ src/services/_index.js
@@ -1,13 +1,16 @@
 import models from '../models/_index.js';
 import UnitService from './unit.js';
-import UserService from "./user.js";
+import UserService from './user.js';
+import UserAccessLogService from './userAccessLog.js';
 
 const unitService = new UnitService(models.Unit);
 const userService = new UserService();
+const userAccessLogService = new UserAccessLogService();
 
 const services = {
   unitService,
   userService,
+  userAccessLogService,
 };
 
 export default services;
