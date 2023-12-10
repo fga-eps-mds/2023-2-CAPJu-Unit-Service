24aa7b5b245fe98faa46f0d89de8c1f525830863
Washington Bispo <juniorbispo515@gmail.com>
2023-12-10 17:44:36 -0300

    Arrumando teste após resolução de conflito

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,16 +1,5 @@
 import { Op } from 'sequelize';
 
-export function filterByNicknameAndRecord(req) {
-  return req.query.filter?.type === 'process'
-    ? {
-        [Op.or]: [
-          { record: { [Op.like]: `%${req.query.filter.value}%` } },
-          { nickname: { [Op.iLike]: `%${req.query.filter.value}%` } },
-        ],
-      }
-    : {};
-}
-
 export function filterByName(req) {
   return req.query.filter?.type === 'unit'
     ? {
@@ -18,12 +7,3 @@ export function filterByName(req) {
       }
     : {};
 }
-
-export function filterByFullName(req) {
-  return req.query.filter?.type === 'user'
-    ? {
-        [Op.or]: [{ fullName: { [Op.iLike]: `%${req.query.filter.value}%` } }],
-      }
-    : {};
-}
-
